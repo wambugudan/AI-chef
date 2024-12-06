2a9f50ca57e7708d03ab7f0a1bf509c0315f057c
Daniel Wambugu <wambugudani@gmail.com>
2024-12-06 16:27:40 +0300

    Updated the toggle function for the sound pads cards

--- my-app/src/components/exe/App.jsx
+++ my-app/src/components/exe/App.jsx
@@ -8,13 +8,19 @@ export default function App(){
 
     const [padsBtn, setPads] = useState(PadsData)
 
-    const toggle = function(){
-        console.log("Clicked!!")
+
+
+    const toggle = function(id){
+        setPads(prevPads => prevPads.map(item => {
+            return item.id === id ? {...item, on: !item.on} : item
+        }))
     }
+
 
     const buttonElement = padsBtn.map(pad => (
         <Pad 
-            key={pad.id} 
+            key={pad.id}
+            id={pad.id} 
             color={pad.color} 
             on={pad.on} 
             btnClicked={toggle} 
--- my-app/src/components/exe/Pad.jsx
+++ my-app/src/components/exe/Pad.jsx
@@ -1,20 +1,14 @@
-import {useState} from "react"
 
 export default function Pad(props){
 
-    const [btnOn, setBtnOn] = useState(props.on)
-
-    // const handleClick = function(){
-    //     setBtnOn(prevBtnOn => !prevBtnOn)
-    // }
 
      return(
         <div>
             <button 
-                className={btnOn ? "pads-btn-on" : "pads-btn-off"} 
+                className={props.on ? "pads-btn-on" : "pads-btn-off"} 
                 key={props.id} 
                 style={{backgroundColor: props.color}}
-                onClick={props.btnClicked}
+                onClick={() => props.btnClicked(props.id)}
             >
                 Btn
             </button>
